e1964719efd8517eecdb3fd4d555e995900bf943
João Vitor <110263732+stormxal@users.noreply.github.com>
2026-03-22 01:33:28 -0300

    Add files via upload

---
+++ calculadora.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+// o compilador gera tudo de uma vez só entao nao funciona corretamente
+// no vscode funciona normal, em outros compiladores deve ser a mesma coisa
+
+int main () {
+
+double number1;
+double number2;
+double resultado = 0;
+char operacao;
+
+cout << "digite o primeiro numero: \n";
+cin >> number1;
+
+cout << "qual a operacao desejada: \n";
+cin >> operacao;
+
+cout << "digite o segundo numero: \n";
+cin >> number2;
+
+if (operacao == '+') {
+resultado = number1 + number2;}
+else if (operacao == '-') {
+resultado = number1 - number2;}
+else if (operacao == '*'){
+resultado = number1 * number2;}
+else if (operacao == '/') {
+    if (number2 == 0) {
+        cout << "Erro: divisao por zero!\n";
+        return 0;
+    }
+resultado = number1 / number2; } 
+else {
+    cout << "Operacao invalida!\n";
+    return 0;
+}
+
+cout << "o resultado é: " << resultado;
+
+}
